6c1127f0279e5209ed2a8a28bed8a91606fa15be
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-05-13 15:18:03 -0300

    config: eslint and editor config

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express from "express";
 
 const server = express();
 
-server.get('/', (request, response) => {
-  return response.send('Hello world!')
-})
+server.get("/", (request, response) => {
+  return response.send("Hello world!");
+});
 
-export { server }; 
+export { server };
