3fa739fa3fa126d5daacce940132d266c5ce5a48
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-05-14 16:29:38 -0300

    feat: body validation
    Building a function to validate any field with any element that will be passed. Also, creating a default variable to the City endpoint url.

---
+++ src/server/controller/city/Create.ts
@@ -0,0 +1,29 @@
+import { Request, RequestHandler, Response, request, response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from "yup";
+import  {paramValidator } from "../../shared/middleware";
+
+interface CityDto {
+  name: string;
+  state: string;
+}
+
+export const createValidation = paramValidator((getSchema) => ({
+  body: getSchema<CityDto>(
+    yup.object().shape({
+      name: yup.string().required().min(3),
+      state: yup.string().required().min(2),
+    })
+  ),
+}));
+
+// export const validateBodyToCreate = validation.paramValidator(createValidation);
+
+export const create = async (
+  request: Request<{}, {}, CityDto>,
+  response: Response
+) => {
+
+console.log(request.body);
+return response.send("Created! - CreateRequest");
+}
---
+++ src/server/controller/index.ts
@@ -0,0 +1,8 @@
+import * as create from './city/Create'
+
+export const urlCityController: string = "/api/v1/city";
+
+
+export const CityController = {
+  ...create
+}
