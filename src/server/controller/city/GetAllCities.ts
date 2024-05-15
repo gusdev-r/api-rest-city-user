0d8c92c71420b6572cf029fb0f0cafb4b1934a7f
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-05-15 10:36:18 -0300

    feat: gettAll endpoint

---
+++ src/server/controller/city/GetAllCities.ts
@@ -0,0 +1,25 @@
+import { StatusCodes } from "http-status-codes";
+import { Request, Response } from "express";
+import { paramValidator } from "../../shared/middleware";
+import * as yup from "yup";
+
+interface QueryProps {
+  page?: number;
+  limit?: number;
+  filter?: string;
+}
+
+export const getAllValidation = paramValidator((getSchema) => ({
+  query: getSchema<QueryProps>(
+    yup.object().shape({
+      page: yup.number().optional().moreThan(0),
+      limit: yup.number().optional().moreThan(0),
+      filter: yup.string().optional(),
+    })
+  ),
+}));
+
+export const getAllCities = async (
+  request : Request<{}, {}, {}, QueryProps>,
+  response: Response) => {
+}
