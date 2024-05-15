317730b91128cd1a3c30a9937d48ebfd3aaa6840
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-05-15 10:44:54 -0300

    feat: getById endpoint

---
+++ src/server/controller/city/GetCityById.ts
@@ -0,0 +1,23 @@
+
+import { Request } from "express";
+import { paramValidator } from "../../shared/middleware";
+import * as yup from "yup";
+
+interface ParamProperty { 
+  id?: number;
+}
+
+export const getCityByIdValidation = paramValidator((getSchema) => ({
+  params: getSchema<ParamProperty>(
+     yup.object().shape({
+      id: yup.number().required().moreThan(0),
+     })
+    )
+}))
+
+export const getCityById = async (
+  request: Request<ParamProperty>,
+  response: Response) => {
+
+    return "";
+}
